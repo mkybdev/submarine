0081746cd87c160c9c98a3e520c49827a8e30406
坂本洸亮 <mituk@Ks-MacBook-Air.local>
2023-07-05 16:29:16 +0900

    Halfway Progress

---
+++ public/connection.js
@@ -0,0 +1,78 @@
+import { getFirestore, connectFirestoreEmulator, onSnapshot, collection, getDoc, setDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import main from './main.js';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDeb13n42OlEVzhv2Hx_Ll04FuZOi0GhCE",
+    authDomain: "mkybdevssubmarine.firebaseapp.com",
+    projectId: "mkybdevssubmarine",
+    storageBucket: "mkybdevssubmarine.appspot.com",
+    messagingSenderId: "108771342371",
+    appId: "1:108771342371:web:3faed3d30ca890c3925996"
+}
+    
+initializeApp(firebaseConfig);
+
+function initializeFirebaseEmulating() {
+    const db = getFirestore();
+    connectFirestoreEmulator(db, 'localhost', 8080);
+    return db;
+}
+
+function initializeFirebase() {
+    const app = initializeApp();
+    return getFirestore(app);
+}
+
+// Initialize Firebase
+const isEmulating = true;
+const db = (isEmulating) ? initializeFirebaseEmulating() : initializeFirebase();
+
+document.getElementById('game').style.display = "none";
+
+document.getElementById('enterRoom').onclick = async function() {
+    const roomID = document.getElementById('roomID').value;
+    const userName = document.getElementById('userName').value;
+    const docRef = doc(db, "rooms", roomID);
+    const querySnapshot = await getDoc(docRef);
+    if (querySnapshot.exists()) {
+        const users = querySnapshot.data().users;
+        users[1] = userName;
+        await updateDoc(docRef, {
+            users: users
+        });
+        document.getElementById('init').style.display = "none";
+        document.getElementById('game').style.display = "block";
+        main(db, roomID, 1);
+    } else {
+        const fieldInitial = {};
+        for (let i = 0; i < 5; i++) {
+            fieldInitial[i] = ["", "", "", "", ""];
+        }
+        try {
+            await setDoc(docRef, {
+                users: {0: userName, 1: ""},
+                ownfield: fieldInitial,
+                oppfield: fieldInitial,
+                turn: 0
+            });
+        } catch (e) {
+            console.error("Error setting document: ", e);
+        }
+        document.getElementById('initMessage').textContent = "Waiting for the opponent...";
+        document.querySelector('.form').style.display = "none";
+        const wait = await onSnapshot(docRef, (doc) => {
+            const users = doc.data().users;
+            if (users[1] !== "") {
+                wait();
+                document.getElementById('init').style.display = "none";
+                document.getElementById('game').style.display = "block";
+                main(db, roomID, 0);
+            }
+        });
+    }
+}
